fb45a4de6ec0d60c1921c7f69c76c4728351d8f3
214G1A0549 <141648975+Lavanya549@users.noreply.github.com>
2023-08-29 13:01:56 +0530

    Add files via upload

--- sjf.c
+++ sjf.c
@@ -1,39 +1,37 @@
 #include<stdio.h>
-#include<conio.h>
-main()
-{
-int p[20], bt[20], wt[20], tat[20], i, k, n, temp; float wtavg,
-tatavg;
-clrscr();
-printf("\nEnter the number of processes -- ");
-scanf("%d", &n);
-for(i=0;i<n;i++)
-{
-p[i]=i;
-printf("Enter Burst Time for Process %d -- ", i);
-scanf("%d", &bt[i]);
+int main(){
+	int i,n,wt[20],bt[20],tat[20],p[20],k,temp;
+	float wtavg,tatavg;
+	printf("Enter the number of process---");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		printf("The bursttime p%d--",i);
+		scanf("%d",&bt[i]);
+	}
+	for(i=1;i<=n;i++){
+		for(k=i+1;k<=n;k++){
+			if(bt[i]>bt[k]){
+				temp=bt[i];
+				bt[i]=bt[k];
+				bt[k]=temp;
+			}
+			temp=p[i];
+			p[i]=p[k];
+			p[k]=temp;
+		}
+	}
+	wt[1]=wtavg=0;
+	tat[1]=tatavg=bt[1];
+	for(i=2;i<=n;i++){
+		wt[i]=wt[i-1]+bt[i-1];
+		tat[i]=tat[i-1]+bt[i];
+		wtavg=wtavg+wt[i];
+		tatavg=tatavg+tat[i];
+	}
+	printf("\nProcess\tBurstTime\tWaiting time\tTurnaround Time\n");
+	for(i=1;i<=n;i++){
+		printf("p%d\t\t%d\t\t%d\t\t%d\n",i,bt[i],wt[i],tat[i]);
+	}
+	printf("Average waiting time--%f\n",wtavg/n);
+	printf("Average turnaround time--%f",tatavg/n);
 }
-for(i=0;i<n;i++)
-for(k=i+1;k<n;k++)
-if(bt[i]>bt[k])
-{
-temp=bt[i];
-bt[i]=bt[k];
-bt[k]=temp;
-temp=p[i];
-p[i]=p[k];
-p[k]=temp;
-}
-wt[0] = wtavg = 0;
-tat[0] = tatavg = bt[0]; for(i=1;i<n;i++)
-{
-wt[i] = wt[i-1] +bt[i-1];
-tat[i] = tat[i-1] +bt[i];
-wtavg = wtavg + wt[i];
-tatavg = tatavg + tat[i];
-}
-printf("\n\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
-for(i=0;i<n;i++)
-printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
-printf("\nAverage Waiting Time -- %f", wtavg/n);
-printf("\nAverage Turnaround Time -- %f", tatavg/n); getch();}
